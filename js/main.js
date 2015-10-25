58188a8ff6a06bc8fccd89ab579f64b82e2291ee
Alikhan Uzakov <alihan-uzakov@mail.ru>
2015-10-25 07:34:52 +0000

    Minor code change
    Changed code and added pictures

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
 
-  var animated_objects = $(".removed");
-
+  var animated_objects = $(".images");
+  var main_animated_objects = $(".swag");
   $("#mlg-off").click(function(e){
     e.preventDefault();
-    animated_objects.removeClass('removed').hide();
+    animated_objects.addClass('hidden');
+    main_animated_objects.removeClass('animated');
+    main_animated_objects.removeClass('infinite');
   });
   $("#mlg-on").click(function(e){
     e.preventDefault();
-    animated_objects.addClass('removed').show();
-
+    animated_objects.removeClass('hidden');
+    main_animated_objects.addClass('animated');
+    main_animated_objects.addClass('infinite');
   });
+
 });
